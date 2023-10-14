e03899cb0d02e2d4192f3e1d26805acad4bc8dff
rasmus1 <rasmus1@uw.edu>
2023-10-14 15:19:13 -0700

    trying to import tailwind

---
+++ client/src/Header.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+export const Header = () => {
+  return (
+    <>
+      <div className="flex flex-row">
+        <img src="meet-together.png" alt="meet together" />
+        <img src="m2-favicon.png" alt="meet together" />
+        <p className="text-3xl font-bold underline">About Us</p>
+      </div>
+      <hr></hr>
+    </>
+  );
+};
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/index.js",
+    "./src/Header.js",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/index.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
