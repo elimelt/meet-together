2434a83f0d43c8c843797f6a3b7d177fd57c2161
simku22 <simku22@uw.edu>
2023-10-15 02:57:25 -0700

    starting work on the radio parser

---
+++ client/src/tools/formParser.js
@@ -0,0 +1,21 @@
+import {
+    getFormMetadata as getMetadata,
+    getFormResponses as getResponses,
+    processedFormdata as processResponses,
+    processedMetadata as processMetadata
+   } from './form.js';
+
+// RADIO
+// DROPDOWN
+// CHECKBOX
+// 
+
+export const radioParser = async () => {
+    const formId = '1COaD0aW1PwPEj2tW4QtQ_IRY3JPK0kc5BulEK8GjADk';
+    const metadata = processMetadata(await getMetadata(formId));
+    const responses = processResponses(await getResponses(formId));
+
+    console.log('metadata: ', metadata);
+    console.log('responses: ', responses);
+    return (<></>);
+}
