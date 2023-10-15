ddb903130a351d8f2586bc2afc9112ce0bde46c4
rasmus1 <rasmus1@uw.edu>
2023-10-14 21:20:10 -0700

    added some text. needs to be formatted

--- client/src/About.js
+++ client/src/About.js
@@ -9,6 +9,9 @@ export const About = () => {
       <h1 className="text-6xl font-bold ml-4">
         MEET TOGETHER
       </h1>
+      <p>
+      With our innovative matching algorithm, we make it easy for users to build their communities by leveraging form data and assigning priority to ideas that matter, ensuring personalized and engaging group dynamics. Join us to effortlessly create connections that matter!
+      </p>
     </div>
   )
 }
