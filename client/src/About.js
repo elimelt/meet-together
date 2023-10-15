d70d8ddd2fbb3efd4451d2720b5a80ea47f61ad3
rasmus1 <rasmus1@uw.edu>
2023-10-14 21:10:11 -0700

    Starting work on the about page. implemented react router

---
+++ client/src/About.js
@@ -0,0 +1,14 @@
+export const About = () => {
+  return (
+    <div id="about" className="flex items-end w-100 h-100 m-auto">
+      <img
+        className="h-20"
+        src="meet-together.png"
+        alt="m2-logo"
+      />
+      <h1 className="text-6xl font-bold ml-4">
+        MEET TOGETHER
+      </h1>
+    </div>
+  )
+}
